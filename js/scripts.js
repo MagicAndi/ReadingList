bbf810a5f851578354f522a36c4ed982e15631cb
Andy Parkhill <a.parkhill@qub.ac.uk>
2017-10-19 11:52:18 +0100

    Refactoring initial reading list to something a bit more nuanced.

--- js/scripts.js
+++ js/scripts.js
@@ -21,149 +21,55 @@ function removeChildNodes(element) {
 }
 
 function showInfo(data, tabletop) {
-  var retrievedData = document.getElementById('retrievedData');
-  removeChildNodes(retrievedData);
-  retrievedData.innerHTML = "";
-
-  var table = document.createElement('table');
-  table.id = "books";
-  var tableHeader = document.createElement('thead');
-  var tableBody = document.createElement('tbody');
-  
-  var headerRow = document.createElement('tr');
-  var columns = data["Read Books"].columnNames; 
-
-  var headerCell = document.createElement('th');
-  headerCell.innerText = "Cover";
-  headerCell.setAttribute('width', '90px');
-  headerRow.appendChild(headerCell);
-  
-  var headerCell = document.createElement('th');
-  headerCell.innerText = "Title";
-  headerRow.appendChild(headerCell);
-
-  var headerCell = document.createElement('th');
-  headerCell.innerText = "Author(s)";
-  headerRow.appendChild(headerCell);
-  
-  var headerCell = document.createElement('th');
-  headerCell.innerText = "Classification";
-  headerRow.appendChild(headerCell);
-  
-  var headerCell = document.createElement('th');
-  headerCell.innerText = "Tags";
-  headerRow.appendChild(headerCell);  
-  
-  var headerCell = document.createElement('th');
-  headerCell.innerText = "Date Read";
-  headerRow.appendChild(headerCell);
-  
-  var headerCell = document.createElement('th');
-  headerCell.innerText = "Rating";
-  headerRow.appendChild(headerCell);
-
-  var headerCell = document.createElement('th');
-  tableHeader.appendChild(headerRow);
-  table.appendChild(tableHeader);
+  var content = document.getElementById('content');
+  removeChildNodes(content);
+  content.innerHTML = "";
 
   var records = data["Read Books"].elements;
-  for (var i = 0; i < records.length; i++){
-    var row = document.createElement('tr');
 
-    // Cover image
-    var cell = document.createElement('td');
-    var image = document.createElement('img');
-    var imageUrl = "https://sites.google.com/site/andyparkhill/home/book-images/" + records[i].ISBN + ".jpg";
-    image.setAttribute('src', imageUrl);
-    image.setAttribute('alt', 'Book cover for ' + records[i].Title + ' by ' + records[i].Author + ' (ISBN: ' + records[i].ISBN + ')');
-    image.setAttribute('width', '80px');
-    cell.appendChild(image);
-    row.appendChild(cell);
+  if(records.length > 0)
+  {
+    // Currently reading
+    var header = document.createElement('h3');
+    header.innerText = "Now Reading";
+    content.appendChild(header);
+    
+    var currentBook = records[0];
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "I'm currently reading " + currentBook.Title + " by " + currentBook.Author + ".";
+    content.appendChild(paragraph);
 
-    // Title
-    var cell = document.createElement('td');
-    cell.innerText = records[i].Title;
-    row.appendChild(cell);
-    
-    // Author
-    var cell = document.createElement('td');
-    cell.innerText = records[i].Author;
-    row.appendChild(cell);
-    
-    // Classification
-    var cell = document.createElement('td');
-    cell.innerText = records[i].Classification;
-    row.appendChild(cell);
-    
-    // Tags
-    var cell = document.createElement('td');
-    cell.innerText = records[i].Tags;
-    row.appendChild(cell);
-    
-    // Date Read
-    var cell = document.createElement('td');
-    var dateRead = records[i].DateRead;
-    console.log("Date read: " + dateRead);
-    console.log("Date read length: " + dateRead.length);
+    // Recently finished
+    var header = document.createElement('h3');
+    header.innerText = "Recently Finished";
+    content.appendChild(header);
+    var list = document.createElement('ul');
 
-    if(dateRead == null || dateRead.length === 0)
-    {
-      cell.innerText = "Currently reading";
-    }
-    else
-    {
-      cell.innerText = dateRead;
-    }
-    row.appendChild(cell);
-    
-    // Rating
-    var cell = document.createElement('td');
-    var rating = records[i].MyRating;
-    imageUrl = "https://sites.google.com/site/andyparkhill/home/book-images/";
-    altText = "";
+    for (var i = 1; i <= 10; i++){
+      var element = document.createElement('li');
+      
+      // Add image
+      // Add header
+      // Add details
+      
+      element.innerText = records[i].Title + " by " + records[i].Author;
+      list.appendChild(element);
 
-    switch (rating) {
-      case '1':
-          imageUrl += "1_stars.png";
-          altText += "Rated as 1 out of 5 stars";
-          break;
-      case '2':
-          imageUrl += "2_stars.png";
-          altText += "Rated as 2 out of 5 stars";
-          break;
-      case '3':
-          imageUrl += "3_stars.png";
-          altText += "Rated as 3 out of 5 stars";
-          break;
-      case '4':
-          imageUrl += "4_stars.png";
-          altText += "Rated as 4 out of 5 stars";
-          break;
-      case '5':
-          imageUrl += "5_stars.png";
-          altText += "Rated as 5 out of 5 stars";
-          break;
-      default:
-        imageUrl = "";
-        cell.innerText = "No rating";
-        console.log('Rating not set!');
+      <img src="http://lorempixum.com/100/100/nature/1" >
+      <h3>The Grasslands</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.</p>
+    </li>
     }
 
-    if(imageUrl.length > 0) {
-      var image = document.createElement('img');
-      image.setAttribute('src', imageUrl);
-      image.setAttribute('alt', altText);
-      image.setAttribute('width', '60px');
-      cell.appendChild(image);
-    }
-
-    row.appendChild(cell);
-
-    tableBody.appendChild(row);
+    content.appendChild(list);
   }
+  else
+  {
+    // Give an error message...
+  }
+
   
-  table.appendChild(tableBody);
-  retrievedData.appendChild(table);
+  content.appendChild(table);
 }
 
 window.addEventListener('DOMContentLoaded', init)
